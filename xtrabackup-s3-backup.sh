7fd02a93337be02dd937e95ceb03b1ef20e60989
dgitman <dgitman@ndap-llc.com>
2015-05-04 21:41:29 +0000

    IT-406

--- xtrabackup-s3-backup.sh
+++ xtrabackup-s3-backup.sh
@@ -1,8 +1,13 @@
 #Backup whole MySQL database instance
-/usr/bin/innobackupex --compress --rsync /backups/database
+/usr/bin/innobackupex --compress --rsync /backups/database > /var/log/xtrabackup 2>&1
+
+if [ -z "`tail -1 $TMPFILE | grep 'completed OK!'`" ] 
+ then
+   echo "$INNOBACKUPEX failed:"
+fi
 
 #Remove backups older than 28 days
-find /backups/database/* -mtime +28 -exec rm -rf {} \;
+find /backups/database -mtime +28 -exec rm -rf {} \;
 
 #Sync to Smazon S3 Bucket
 aws s3 sync /backups s3://ndap-etl-backup --delete --only-show-errors
