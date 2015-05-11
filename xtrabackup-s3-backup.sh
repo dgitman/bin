bcf2bd3f355de51675b1f79f91a26ccc48d8b06d
David Gitman <dgitman@ndap-llc.com>
2015-05-11 13:59:41 -0400

    Update xtrabackup-s3-backup.sh

--- xtrabackup-s3-backup.sh
+++ xtrabackup-s3-backup.sh
@@ -7,7 +7,7 @@ if [ -z "`tail -1 $TMPFILE | grep 'completed OK!'`" ]
 fi
 
 #Remove backups older than 28 days
-find /backups/database -mtime +28 -exec rm -rf {} \;
+find /backups/database -prune -mtime +7 -exec rm -rf {} \;
 
 #Sync to Smazon S3 Bucket
 aws s3 sync /backups s3://ndap-etl-backup --delete --only-show-errors
