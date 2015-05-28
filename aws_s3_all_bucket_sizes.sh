d98227141786e6a862ebda96b908d9e20648da29
dgitman <dgitman>
2015-05-28 16:28:40 -0400

    Beautify aws_s3_all_bucket_sizes

--- aws_s3_all_bucket_sizes.sh
+++ aws_s3_all_bucket_sizes.sh
@@ -2,8 +2,8 @@
 buckets=`s3cmd ls | awk '{FS=" ";print $3}'`
 for bucket in $buckets
 do
-size=`s3cmd du "$bucket" |awk '{FS=" ";print $1}'`
-sizemb=`expr $size / \\( 1024 \\* 1024 \\)`
-sizegb=`expr $sizemb / 1024`
-echo "$bucket ${sizegb} GB ${sizemb} MB ${size} bytes"
+  size=`s3cmd du "$bucket" |awk '{FS=" ";print $1}'`
+  sizemb=`expr $size / \\( 1024 \\* 1024 \\)`
+  sizegb=`expr $sizemb / 1024`
+  echo "$bucket ${sizegb} GB ${sizemb} MB ${size} bytes"
 done
