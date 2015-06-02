b272f9a47cc4dd2a7c79c25399b589ef973d9e0b
dgitman <dgitman@ndap-llc.com>
2015-06-02 18:06:39 +0000

    beautify bash

--- mysql-backup-s3.sh
+++ mysql-backup-s3.sh
@@ -23,36 +23,37 @@ echo -e "Dumping to \e[1;32m$bucket/$stamp/\e[00m"
 
 # Loop the databases
 for db in $databases; do
-
+  
   # Define our filenames
   filename="$stamp - $db.sql.gz"
   tmpfile="/tmp/$filename"
   object="$bucket/$stamp/$filename"
-
+  
   # Feedback
   echo -e "\e[1;34m$db\e[00m"
   
-    # List all the tables
+  # List all the tables
   tables=`mysql $db -u root -p$mysqlpass -e "SHOW TABLES;" | tr -d "| "`
- 
+  
   for tb in $tables; do
     # Define our filenames
     filename="$db.$tb.sql.gz"
     tmpfile="/tmp/$filename"
     object="$bucket/$stamp/$filename"
-
-  # Dump and zip
-  echo -e "  creating \e[0;35m$tmpfile\e[00m"
-  mysqldump --opt --max_allowed_packet=512M --databases "$db" --table "$tb" | gzip -c > "$tmpfile"
-
-  # Upload
-  echo -e "  uploading..."
-  s3cmd put "$tmpfile" "$object"
-
-  # Delete
-  rm -f "$tmpfile"
-
-done;
-
-# Jobs a goodun
-echo -e "\e[1;32mJobs a goodun\e[00m"
+    
+    # Dump and zip
+    echo -e "  creating \e[0;35m$tmpfile\e[00m"
+    mysqldump --opt --max_allowed_packet=512M --databases "$db" --table "$tb" | gzip -c > "$tmpfile"
+    
+    # Upload
+    echo -e "  uploading..."
+    s3cmd put "$tmpfile" "$object"
+    
+    # Delete
+    rm -f "$tmpfile"
+    
+  done;
+  
+  # Jobs a goodun
+  echo -e "\e[1;32mJobs a goodun\e[00m"
+  
