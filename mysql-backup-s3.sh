e90188890eaf29fba0a67a06aca47132ddee88a6
dgitman <dgitman@ndap-llc.com>
2015-06-03 20:34:53 +0000

    more mysql-backup-s3 cleanup

--- mysql-backup-s3.sh
+++ mysql-backup-s3.sh
@@ -1,59 +1,77 @@
 #!/bin/bash
+# ------------------------------------------------------------------
+#                  MySQL Backup to Amazon S3
+#        This backups all MySQL datatbases on a per tables basis to Amazon S3
+# ------------------------------------------------------------------
 
-# Be pretty
-echo -e " "
-echo -e " .  ____  .    ______________________________"
-echo -e " |/      \|   |                              |"
-echo -e "[| \e[1;31mâ™¥    â™¥\e[00m |]  | S3 MySQL Backup Script v.0.1 |"
-echo -e " |___==___|  /                Â© oodavid 2012 |"
-echo -e "              |______________________________|"
-echo -e " "
+# -----------------------------------------------------------------
+#  Describing variables
+# -----------------------------------------------------------------
 
-# Basic variables
 bucket="s3://ndap-etl-database-backup"
+stamp=`date -Iseconds`
+IFS=$(echo -en "\n\b")
 
-# Timestamp (sortable AND readable)
-stamp=`date --rfc-3339=seconds`
+# -----------------------------------------------------------------
+#  Log start of MySQL backup to Amazon S3
+# -----------------------------------------------------------------
 
-# List all the databases
-databases=`mysql -e "SHOW DATABASES;" | tr -d "| " | grep -v "\(Database\|information_schema\|performance_schema\|mysql\|test\)"`
+echo "Starting MySQL backup to Amazon S3 bucket $bucket/$stamp"
 
-# Feedback
-echo -e "Dumping to \e[1;32m$bucket/$stamp/\e[00m"
+# -----------------------------------------------------------------
+#  List all databases
+# -----------------------------------------------------------------
 
-# Loop the databases
+databases=`mysql --batch --skip-column-names -e "SHOW DATABASES;" | grep -v "\(Database\|information_schema\|performance_schema\|mysql\|test\)"`
+
+# -----------------------------------------------------------------
+#  Loop through the databases
+# -----------------------------------------------------------------
+
 for db in $databases; do
   
-  # Define our filenames
-  filename="$stamp - $db.sql.gz"
+  filename="$db.routines.sql.gz"
   tmpfile="/tmp/$filename"
   object="$bucket/$stamp/$filename"
   
-  # Feedback
-  echo -e "\e[1;34m$db\e[00m"
+  # -----------------------------------------------------------------
+  #  mysqldump routines (stored procedures/functions)
+  # -----------------------------------------------------------------
   
-  # List all the tables
-  tables=`mysql $db -u root -p$mysqlpass -e "SHOW TABLES;" | tr -d "| "`
+  mysqldump --routines --no-create-info --no-data --no-create-db --skip-opt "$db" | gzip -c > "$tmpfile"
+  s3cmd -q put "$tmpfile" "$object"
+  rm -f "$tmpfile"
+  
+  # -----------------------------------------------------------------
+  #  List all tables
+  # -----------------------------------------------------------------
+  
+  tables=`mysql --batch --skip-column-names $db -e "SHOW TABLES;"`
+  
+  # -----------------------------------------------------------------
+  #  Loop through the tables
+  # -----------------------------------------------------------------
   
   for tb in $tables; do
-    # Define our filenames
     filename="$db.$tb.sql.gz"
     tmpfile="/tmp/$filename"
     object="$bucket/$stamp/$filename"
+    echo -e "$db"."$tb"
     
-    # Dump and zip
-    echo -e "  creating \e[0;35m$tmpfile\e[00m"
-    mysqldump --opt --max_allowed_packet=512M --databases "$db" --table "$tb" | gzip -c > "$tmpfile"
+    # -----------------------------------------------------------------
+    #  mysqldump tables
+    # -----------------------------------------------------------------
     
-    # Upload
-    echo -e "  uploading..."
-    s3cmd put "$tmpfile" "$object"
-    
-    # Delete
+    mysqldump --opt --max_allowed_packet=512M --databases "$db" --tables "$tb" | gzip -c > "$tmpfile"
+    s3cmd -q put "$tmpfile" "$object"
     rm -f "$tmpfile"
     
   done;
   
-  # Jobs a goodun
-  echo -e "\e[1;32mJobs a goodun\e[00m"
-  
+done;
+
+# -----------------------------------------------------------------
+# Log completion of MySQL backup to Amazon S3
+# -----------------------------------------------------------------
+
+echo "Completed MySQL xibBackup to Amazon S3 bucket $bucket/$stamp"
